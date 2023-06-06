78d85da97fa4fd5f894d24e3442c10724e1230f8
joas-barros <joas.barros14@hotmail.com>
2023-06-06 10:46:03 -0300

    add binary tree

---
+++ Arvore_Binaria.py
@@ -0,0 +1,21 @@
+class no:
+
+    def __int__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
+
+    def getvalor(self):
+        return self.valor
+
+    def setesquerda(self, esquerda):
+        self.esquerda = esquerda
+
+    def setdireita(self, direita):
+        self.direita = direita
+
+    def getesquerda(self):
+        return self.esquerda
+
+    def getdireita(self):
+        return self.direita
